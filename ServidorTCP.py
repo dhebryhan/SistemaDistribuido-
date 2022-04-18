6a3fbb148b09fdad8bd0cd163e9b1d7576b9c824
dhebryhan <35630829+dhebryhan@users.noreply.github.com>
2022-04-18 18:58:19 -0300

    Update ServidorTCP.py

--- ServidorTCP.py
+++ ServidorTCP.py
@@ -1,25 +1,25 @@
-
 from socket import *
 
-serverPort = 9999
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
-serverSocket.listen(5)
+portaServidor = 9999
+servidorSocket = socket(AF_INET, SOCK_STREAM)
+servidorSocket.bind(('', portaServidor)) #para qual porta deve esperar conexao.
+servidorSocket.listen(5) #coloca o socket no modo servidor e limite de conexao.
 print('Funcionando')
 
 
 while 1:
-    connectionSocket, addr = serverSocket.accept()
-    nomeArquivo = connectionSocket.recv(1024)
+    conexaoSocket, addr = servidorSocket.accept() #Deixa o servidor na escuta.
+    nomeArquivo = conexaoSocket.recv(1024)
     try:
         arq = open(nomeArquivo.decode()+'.txt', 'r')
-        for i in arq.readlines():
-            connectionSocket.send(i.encode())
-        arq.close()
+        for i in arq.readlines(): #retorna uma string no arquivo.
+            conexaoSocket.send(i.encode())
+        arq.close() #fecha o arquivo.
     except Exception as e:
         print(e)
-        connectionSocket.send("Arquivo nao existe".encode())
+        conexaoSocket.send("Arquivo nao existe".encode())
         
-    connectionSocket.close()
+    conexaoSocket.close()
 
+ 
  
